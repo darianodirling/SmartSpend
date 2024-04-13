033f27787322bfd7be69b5075d3c531af446a263
jethrolibutan <jethrolibutan@gmail.com>
2024-04-13 14:42:08 -0400

    writing query to figure out whats in the database...

---
+++ script.py
@@ -0,0 +1,55 @@
+from sqlalchemy import create_engine, MetaData, select, column, Float
+import os
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
+database_path = os.path.join(script_directory, 'instance', 'database.db')
+
+engine = create_engine('sqlite:///' + database_path)
+
+metadata = MetaData()
+
+metadata.reflect(bind=engine)
+
+table_names = metadata.tables.keys()
+
+print("Table Names:")
+for table_name in table_names:
+    print(table_name)
+
+user_table = metadata.tables['user']
+n
+connection = engine.connect()
+
+# Select all usernames from the user table
+select_query = select(user_table.columns.username)
+
+# Execute the query
+result = connection.execute(select_query)
+
+# Fetch all the usernames
+usernames = result.fetchall()
+
+print ("\nUsernames:")
+for username in usernames:
+    print(username[0])
+
+# Get the user table
+user_table = metadata.tables['expense']
+
+# Create a connection
+connection = engine.connect()
+
+# Select all savings from the user table
+select_query = select(user_table.columns.owner)
+
+# Execute the query
+result = connection.execute(select_query)
+
+# Fetch all the usernames
+usernames = result.fetchall()
+
+print ("\nSavings:")
+for username in usernames:
+    print(username[0])
+
