12dd0a043d845ed8ac7d61d86191b6ee9b76db01
jethrolibutan <jethrolibutan@gmail.com>
2024-04-13 13:26:17 -0400

    expense is now being saved to the backend

--- website/views.py
+++ website/views.py
@@ -63,23 +63,6 @@ def create_expense():
     return render_template('start.html', user=current_user)
 
 
-@views.route("/delete-expense/<id>")
-@login_required
-def delete_expense(id):
-    expense = Expense.query.filter_by(id=id).first()
-
-    if not expense:
-        flash("Expense does not exist.", category='error')
-    elif current_user.id != expense.id:
-        flash('You do not have permission to delete this expense.', category='error')
-    else:
-        db.session.delete(expense)
-        db.session.commit()
-        flash('Expense deleted.', category='success')
-
-    return redirect(url_for('views.home'))
-
-
 @views.route("/expenses/<username>")
 @login_required
 def expenses(username):
@@ -136,57 +119,4 @@ from flask import jsonify
 #         db.session.rollback()
 #         return jsonify({"error": str(e)}), 500
 
-# @views.route("/create-comment/<post_id>", methods=['POST'])
-# @login_required
-# def create_comment(post_id):
-#     text = request.form.get('text')
 
-#     if not text:
-#         flash('Comment cannot be empty.', category='error')
-#     else:
-#         post = Post.query.filter_by(id=post_id)
-#         if post:
-#             comment = Comment(
-#                 text=text, author=current_user.id, post_id=post_id)
-#             db.session.add(comment)
-#             db.session.commit()
-#         else:
-#             flash('Post does not exist.', category='error')
-
-#     return redirect(url_for('views.home'))
-
-
-# @views.route("/delete-comment/<comment_id>")
-# @login_required
-# def delete_comment(comment_id):
-#     comment = Comment.query.filter_by(id=comment_id).first()
-
-#     if not comment:
-#         flash('Comment does not exist.', category='error')
-#     elif current_user.id != comment.author and current_user.id != comment.post.author:
-#         flash('You do not have permission to delete this comment.', category='error')
-#     else:
-#         db.session.delete(comment)
-#         db.session.commit()
-
-#     return redirect(url_for('views.home'))
-
-
-# @views.route("/like-post/<post_id>", methods=['POST'])
-# @login_required
-# def like(post_id):
-#     post = Post.query.filter_by(id=post_id).first()
-#     like = Like.query.filter_by(
-#         author=current_user.id, post_id=post_id).first()
-
-#     if not post:
-#         return jsonify({'error': 'Post does not exist.'}, 400)
-#     elif like:
-#         db.session.delete(like)
-#         db.session.commit()
-#     else:
-#         like = Like(author=current_user.id, post_id=post_id)
-#         db.session.add(like)
-#         db.session.commit()
-
-#     return jsonify({"likes": len(post.likes), "liked": current_user.id in map(lambda x: x.author, post.likes)})
